33fb1aff9eccd11218e1bfe4a76d442a7fbc9dc1
azevedorafael <you@example.com>
2020-04-15 00:21:04 -0300

    Create and List transactions

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -5,6 +5,27 @@ interface Balance {
   outcome: number;
   total: number;
 }
+interface CreateTransactionDTO {
+  title: string;
+
+  value: number;
+
+  type: 'income' | 'outcome';
+}
+
+function agruparPor(
+  objetoArray: CreateTransactionDTO[],
+  propriedade: string,
+): any {
+  return objetoArray.reduce(function (acc: any, obj: any) {
+    const key = obj[propriedade];
+    if (!acc[key]) {
+      acc[key] = [];
+    }
+    acc[key].push(obj);
+    return acc;
+  }, {});
+}
 
 class TransactionsRepository {
   private transactions: Transaction[];
@@ -14,15 +35,42 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const groupByType = agruparPor(this.transactions, 'type');
+
+    const { outcome, income } = groupByType;
+    const outcomeSum = outcome.reduce(function (
+      total: number,
+      currentValue: CreateTransactionDTO,
+    ) {
+      return total + currentValue.value;
+    },
+    0);
+    const incomeSum = income.reduce(function (
+      total: number,
+      currentValue: CreateTransactionDTO,
+    ) {
+      return total + currentValue.value;
+    },
+    0);
+    const newObj = {
+      income: incomeSum,
+      outcome: outcomeSum,
+      total: incomeSum - outcomeSum,
+    };
+
+    return newObj;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
